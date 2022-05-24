42c734bc6ff1c105466871a988d70696159adab3
sasha <elshurimem1337@gmail.com>
2022-05-24 21:46:00 +0300

    ку

---
+++ lr34/Header.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef header
+#define header
+
+#include <iostream>
+#include <malloc.h>
+#include <conio.h>
+#include <locale.h>
+#include <string>
+
+
+
+using namespace std;
+
+#endif header
